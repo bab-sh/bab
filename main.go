25fc54d6edb3f2921b7f8b70dc15e5964883723a
AIO <Sebastian-Stepper@gmx.de>
2025-10-03 16:21:27 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/bab/bab/cmd"
+	"github.com/charmbracelet/log"
+)
+
+func main() {
+	logger := log.NewWithOptions(os.Stderr, log.Options{
+		ReportCaller:    false,
+		ReportTimestamp: false,
+		Level:           log.InfoLevel,
+	})
+
+	if os.Getenv("NO_COLOR") != "" {
+		styles := log.DefaultStyles()
+		for i := range styles.Levels {
+			styles.Levels[i] = styles.Levels[i].UnsetBackground().UnsetForeground().UnsetBold()
+		}
+		styles.Key = styles.Key.UnsetForeground()
+		styles.Value = styles.Value.UnsetForeground()
+		logger.SetStyles(styles)
+	}
+
+	log.SetDefault(logger)
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatal("Error", "err", err)
+	}
+}
