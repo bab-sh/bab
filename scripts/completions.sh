a0b021eca54d9a402aa3549f0cf0dc350bdab46c
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2025-12-02 00:08:21 +0100

    fix: completion shell script for github workflow used old sub command

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish powershell; do
-	go run . completion "$sh" >"completions/bab.$sh"
+	go run . --completion "$sh" >"completions/bab.$sh"
 done
